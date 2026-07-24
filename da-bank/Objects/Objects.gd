4492390b4a5575746f46fc997dab0d657511858a
Chill-Cube <212706581+Chill-Cube@users.noreply.github.com>
2026-07-24 20:19:17 +1000

    line

--- da-bank/Objects/Objects.gd
+++ da-bank/Objects/Objects.gd
@@ -6,6 +6,11 @@ class_name PickUpObject
 @export var ground_friction := 2500.0
 @export var stop_threshold := 10.0
 
+var trajectory_steps := 40
+var trajectory_step_time := 1.0 / 60.0
+
+@onready var trajectory_line: Line2D = $TrajectoryLine
+
 var holder: Player = null
 var last_holder: Player = null
 var offset := Vector2(0, 200)
@@ -29,6 +34,7 @@ func pick_up(player: Player, object: Node2D) -> void:
 	velocity = Vector2.ZERO
 
 	$CollisionShape2D.disabled = true
+	trajectory_line.visible = true
 
 func put_down(player: Player, object: Node2D) -> void:
 	if object != self:
@@ -44,6 +50,7 @@ func put_down(player: Player, object: Node2D) -> void:
 	add_collision_exception_with(player)
 
 	velocity = aim_direction * throw_force + player.velocity
+	trajectory_line.visible = false
 
 func _physics_process(delta: float) -> void:
 	if holder != null:
@@ -53,6 +60,7 @@ func _physics_process(delta: float) -> void:
 		if dir.length_squared() > 0.000001:
 			aim_direction = dir.normalized()
 
+		_update_trajectory_preview(holder)
 		return
 
 	if not thrown:
@@ -73,3 +81,33 @@ func _physics_process(delta: float) -> void:
 		if global_position.distance_squared_to(last_holder.global_position) > 64 * 64:
 			remove_collision_exception_with(last_holder)
 			last_holder = null
+
+
+func _update_trajectory_preview(player: Player) -> void:
+	var sim_pos := global_position
+	var sim_vel := aim_direction * throw_force + player.velocity
+	var gravity := get_gravity()
+
+	var points := PackedVector2Array()
+	points.append(sim_pos - global_position)
+
+	var space_state := get_world_2d().direct_space_state
+	var query_exclude := [self, player]
+
+	for i in trajectory_steps:
+		var prev_pos := sim_pos
+		sim_vel += gravity * trajectory_step_time
+		sim_pos += sim_vel * trajectory_step_time
+
+		var query := PhysicsRayQueryParameters2D.create(prev_pos, sim_pos)
+		query.exclude = query_exclude
+		query.collision_mask = collision_mask
+
+		var hit := space_state.intersect_ray(query)
+		if hit:
+			points.append(hit.position - global_position)
+			break
+
+		points.append(sim_pos - global_position)
+
+	trajectory_line.points = points
