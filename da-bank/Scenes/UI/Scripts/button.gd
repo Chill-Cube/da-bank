31ba2cf83812544822d58990b90f803fe930c072
Chill-Cube <212706581+Chill-Cube@users.noreply.github.com>
2026-07-26 22:23:11 +1000

    fixed

--- da-bank/Scenes/UI/Scripts/button.gd
+++ da-bank/Scenes/UI/Scripts/button.gd
@@ -2,8 +2,6 @@ extends Button
 
 func _on_pressed() -> void:
 	GameState.change_state(GameState.State.ENTERING)
-	get_tree().change_scene_to_file("res://Scenes/Gameplay/test.tscn")
-
-
-func _on_button_2_pressed() -> void:
-	get_tree().quit()
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
+	get_tree().change_scene_to_file("res://Scenes/Gameplay/Game.tscn")
